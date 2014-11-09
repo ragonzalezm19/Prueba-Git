3859e4d996f07306ec358693d3c6538cff77dc56
Roberto González <ragonzalezm.19@gmail.com>
2014-11-08 19:33:11 -0430

    Nuevo commit, index y styles modificados

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Prueba</title>
+	<link rel="stylesheet" href="">
+</head>
+<body>
+	<div>
+		<h1>
+			Hola Mundo
+		</h1>
+	</div>
+</body>
+</html>
